313a292475fb6d6e5c3f463f66a802b6e37eb03b
christyheaton <christyheaton@gmail.com>
2022-12-17 19:49:27 -0800

    Add error handling and print statements

--- day1.py
+++ day1.py
@@ -1,16 +1,26 @@
-def get_intput_data(file):
-    """Takes in a file, returns the data as a list"""
-    with open(file) as f:
-        lines = f.readlines()
-        cal_str = '|'.join(lines)
-    return cal_str
+import sys
 
 
-def get_total_per_elf(cal_str):
-    """Takes in a string of calories, returns a list of total calories per elf"""
+def get_input_data(file):
+    """Takes in a file, returns its contents as a list
+    If the file is not found, a test list is returned"""
+    try:
+        with open(file) as f:
+            lines = f.readlines()
+        return lines
+    except FileNotFoundError:
+        print('File not found. Using backup testing input.')
+        return ['1000', '2000', '3000', '', '4000', '', '5000', '6000', '', '7000', '8000', '9000', '', '10000']
+    except Exception as e:
+        print(f'An unexpected error occurred: {e}')
+        sys.exit()
+
+
+def get_total_per_elf(elf_calorie_list):
+    """Takes in a list of calories, returns a list of total calories per elf"""
     sum_cals_elf = []
     count = 0
-
+    cal_str = '|'.join(elf_calorie_list)
     for i in cal_str.split('|'):
         if i.replace('\n', ''):
             count += int(i)
@@ -21,25 +31,29 @@ def get_total_per_elf(cal_str):
     return sum_cals_elf
 
 
-def get_max(cals_list):
-    """Takes in a list of total calories per elf, returns the maximum value"""
-    return max(cals_list)
-
-
-def get_total_top_3(cals_list):
-    """Takes in a list of total calories per elf, returns the sum of the top 3"""
-    cals_list.sort()
-    top_3 = cals_list[-3:]
-    sum_top_3 = 0
-    for n in top_3:
-        sum_top_3 += n
-    return sum_top_3
+def get_total_top_elves(total_calorie_list, num_elves_to_sum):
+    """Takes in a list of total calories per elf and a number of elves
+    returns the sum of the calorie counts for the inputted number elves
+    with the highest calorie counts"""
+    total_calorie_list.sort()
+    top_elves = total_calorie_list[-num_elves_to_sum:]
+    sum_top_elves = 0
+    for elf_cals in top_elves:
+        sum_top_elves += elf_cals
+    return sum_top_elves
 
 
 if __name__ == '__main__':
-    data = get_intput_data('data/day1_input.txt')
+    print('Getting input data...')
+    data = get_input_data('data/day1_input.txt')
+    print('Calculating total calories per elf...')
     total_p_elf = get_total_per_elf(data)
-    max_cals = get_max(total_p_elf)
-    total_top_3 = get_total_top_3(total_p_elf)
-    print(f'The elf with the most calories has {max_cals} calories.')
-    print(f'The 3 elves with the most calories together have {total_top_3} calories.')
+
+    print('Getting the calorie count for the elf with the most calories...')
+    max_cals = max(total_p_elf)
+    print(f'The elf with the most calories has ***{max_cals}*** calories!')
+
+    elf_count = 3
+    print(f'Getting the calorie count for the top {elf_count} elves with the most calories...')
+    total_top_elves = get_total_top_elves(total_p_elf, elf_count)
+    print(f'The {elf_count} elves with the most calories together have a total of ***{total_top_elves}*** calories!')
