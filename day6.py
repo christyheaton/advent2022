eb365685d8bbb50958b2a7e96d1a2ffdb12b8906
christyheaton <christyheaton@gmail.com>
2022-12-21 09:46:47 -0800

    Rename packet length variable

--- day6.py
+++ day6.py
@@ -3,13 +3,12 @@ from aocd import get_data
 
 if __name__ == '__main__':
     data = get_data(day=6, year=2022)
-    unique_count = 4  # use 4 for part 1, 14 for part 2
+    packet_length = 4  # use 4 for part 1, 14 for part 2
     index = 0
     while index < len(data):
-        sub = data[index:index + unique_count]
-        sub_set = set(sub)
-        if len(sub_set) == unique_count:
-            print(f'First unique set of {unique_count} characters is {"".join(sub_set)}')
-            print(f'Found ending at index {index + unique_count}')
+        sub_set = set(data[index:index + packet_length])
+        if len(sub_set) == packet_length:
+            print(f'First unique set of {packet_length} characters is {"".join(sub_set)}')
+            print(f'Found ending at index {index + packet_length}')
             break
         index += 1
