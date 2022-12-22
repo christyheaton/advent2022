2121650aea84de652853ad21397699c147ea2fc9
christyheaton <christyheaton@gmail.com>
2022-12-22 14:47:00 -0800

    Functioning Directory and FileSystem classes

--- day7.py
+++ day7.py
@@ -20,27 +20,18 @@ $ ls
 1111 nono.cat
 8765 molly.cat"""
 
+
 class Directory:
     def __init__(self, name, parent):
         self.name = name
-        self.parent_dir = parent
+        self.parent = parent
+        if self.parent:
+            print(f"Constructor for new dir named: {name}, parent: {parent.name}")
+        else:
+            print(f"Constructor for new dir named: {name}, parent: None")
         self.sub_dirs = {}
         self.files = {}
         self.total_dir_size = 0
-
-    def parse_ls(self, contents):
-        print(f'Received this LS output:\n{contents}')
-        lines = contents.splitlines()
-        for line in lines:
-            if line.startswith('dir'):
-                dir_name = line.split()[1]
-                self.sub_dirs[dir_name] = Directory(parent=self)
-            elif line[0].isnumeric():
-                size, name = line.split()
-                self.files[name] = int(size)
-                print(f'Populating files dict with "{name}" size: {size}')
-            else:
-                raise ValueError(f'Unknown input type: {line}')
 
     def get_size_recursive(self):
         self.total_dir_size = sum(self.files.values())
@@ -48,42 +39,46 @@ class Directory:
             self.total_dir_size += self.sub_dirs.get(key).get_size_recursive()
         return self.total_dir_size
 
+
 class FileSystem:
-    def __init__(self, input):
+    def __init__(self, input=test_data):
         self.input = input
         self.directories = []
-        self.cwd = None
-    def parseInput(self):
+        self.cwd = Directory(parent=None, name="/")
+        self.directories.append(self.cwd)
+        self.parse_input()
+
+    def parse_input(self):
         # This will parse through data in Day7 format and construct a file system
         # tree based on all the cd and ls commands :D :D :D
         commands = test_data.split('$ ')
         size = 0
         for c in commands:
-            print(f'for c Current dir {self.cwd}')
+            print(f'cwd: {self.cwd.name}, command: {c}')
             if c.startswith('ls'):
-                print(f'c. starts with Current dir {self.cwd}')
                 self.parse_ls(c[3:])
             elif c.startswith('cd ..'):
-                self.cwd = self.cwd.parent_dir
+                self.cwd = self.cwd.parent
             elif c.startswith('cd'):
                 print(f'Entering change directory with line {c}')
                 target_name = c.split()[1]
-                if target_name == '/':
-                    self.cwd = Directory(parent=None, name="/")
-                    self.directories.append(self.cwd)
+                if "/" in target_name:
+                    print("skipping / dir")
+                    continue
                 else:
                     self.cwd = self.cwd.sub_dirs.get(c.split()[1])
+                    print(f"new cwd: {self.cwd.name}")
 
     def parse_ls(self, contents):
-        print(f'Parsing "ls" command with lines:\n{contents}')
+        print(f"parsing 'ls' for cwd: {self.cwd.name}")
         lines = contents.splitlines()
         for line in lines:
             if line.startswith('dir'):
                 dir_name = line.split()[1]
                 print(f"Parsing line: '{line}': found new dir to create: {dir_name}")
                 new_dir = Directory(parent=self.cwd, name=dir_name)
+                self.directories.append(new_dir)
                 self.cwd.sub_dirs[dir_name] = new_dir
-                self.directories.append(self.cwd)
             elif line[0].isnumeric():
                 print(f"Parsing line: {line}")
                 size, name = line.split()
@@ -93,27 +88,9 @@ class FileSystem:
             else:
                 raise ValueError(f'Unknown input type: {line}')
 
+
 if __name__ == '__main__':
     data = get_data(day=7, year=2022)
-    test_data = data#[0:500]
-    current_dir = None
-    commands = test_data.split('$ ')
-    size = 0
-    for c in commands:
-        print(f'for c Current dir {current_dir}')
-        if c.startswith('ls'):
-            print(f'c. starts with Current dir {current_dir}')
-            current_dir.parse_ls(c[3:])
-            if current_dir.get_size_recursive() <= 100_000:
-                size += current_dir.get_size_recursive()
-        elif c.startswith('cd ..'):
-            current_dir = current_dir.parent_dir
-        elif c.startswith('cd'):
-            print(f'Entering change directory with line {c}')
-            target_name = c.split()[1]
-            if target_name == '/':
-                current_dir = Directory(parent=None)
-            else:
-                current_dir = current_dir.sub_dirs.get(c.split()[1])
-
-    print(size)
+    test_data = data  # [0:500]
+    f = FileSystem(test_data)
+    print(f)
