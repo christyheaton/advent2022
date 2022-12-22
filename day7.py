2e6e04241dc26a354ed5d68603d4ef8d510b2e51
christyheaton <christyheaton@gmail.com>
2022-12-22 05:54:52 -0800

    Added counter, but result is too high

--- day7.py
+++ day7.py
@@ -6,10 +6,10 @@ class Directory:
         self.parent_dir = parent
         self.sub_dirs = {}
         self.files = {}
-        self.total_size = 0
+        self.total_dir_size = 0
 
     def parse_ls(self, contents):
-        print(f"Received this LS output:\n{contents}")
+        print(f'Received this LS output:\n{contents}')
         lines = contents.splitlines()
         for line in lines:
             if line.startswith('dir'):
@@ -18,30 +18,33 @@ class Directory:
             elif line[0].isnumeric():
                 size, name = line.split()
                 self.files[name] = size
-                self.total_size += size
-                print(f'total size: {self.total_size}')
-                print(f"Populating files dict with '{name}' size: {size}")
+                print(f'Populating files dict with "{name}" size: {size}')
+                self.total_dir_size += int(size)
             else:
                 raise ValueError(f'Unknown input type: {line}')
 
 
 if __name__ == '__main__':
     data = get_data(day=7, year=2022)
-
-    test_data = data[0:300]
-    currentDir = None
+    count_size = 0
+    test_data = data#[0:300]
+    current_dir = None
     commands = test_data.split('$ ')
     for c in commands:
-        print(f'for c Current dir {currentDir}')
+        print(f'for c Current dir {current_dir}')
         if c.startswith('ls'):
-            print(f'c. starts with Current dir {currentDir}')
-            currentDir.parse_ls(c[3:])
+            print(f'c. starts with Current dir {current_dir}')
+            current_dir.parse_ls(c[3:])
+            if current_dir.total_dir_size < 100_000:
+                print(f'Total size of dir is {current_dir.total_dir_size}. Adding to total...')
+                count_size += current_dir.total_dir_size
         elif c.startswith('cd ..'):
-            currentDir = currentDir.parent_dir
+            current_dir = current_dir.parent_dir
         elif c.startswith('cd'):
             print(f'Entering change directory with line {c}')
             target_name = c.split()[1]
             if target_name == '/':
-                currentDir = Directory(parent=None)
+                current_dir = Directory(parent=None)
             else:
-                currentDir = currentDir.sub_dirs.get(c.split()[1])
+                current_dir = current_dir.sub_dirs.get(c.split()[1])
+    print(f'Total count is {count_size}')
