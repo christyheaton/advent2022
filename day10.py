6a7f49bb4d012d634a603df01bfc5fe8a11aeb27
christyheaton <christyheaton@gmail.com>
2022-12-30 08:31:20 -0800

    Part 1 solution

--- day10.py
+++ day10.py
@@ -9,27 +9,29 @@ class Cycle:
     instruction: str
 
 
+def add_cycle(instruction_line):
+    return cycles.append(Cycle(count=cycle, x=x, instruction=instruction_line))
+
+
 if __name__ == '__main__':
     input_data = get_data(day=10, year=2022).splitlines()
     cycles = []
     cycle = 0
     x = 1
+    
     for line in input_data:
         cycle += 1
         if line == 'noop':
-            cycles.append(Cycle(count=cycle, x=x, instruction=line))
+            add_cycle(line)
             continue
         instruction, increment = line.split()
-        cycles.append(Cycle(count=cycle, x=x, instruction=f'{instruction} {increment}'))
+        add_cycle(line)
         cycle += 1
+        add_cycle(line)
         x += int(increment)
-        cycles.append(Cycle(count=cycle, x=x, instruction=f'{instruction} {increment}'))
 
     part1 = 0
     for c in cycles:
         if c.count in [20, 60, 100, 140, 180, 220]:
-            print(f'Adding {c.count} * {c.x} = {c.count * c.x}')
             part1 += (c.count * c.x)
-
-    print(part1)
-    print(cycles)
+    print(f'Part 1 answer is {part1}.')
