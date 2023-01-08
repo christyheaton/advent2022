83f9e2176aa0b36c5c7cd2ce205d7468f1f2282d
christyheaton <christyheaton@gmail.com>
2023-01-08 14:36:14 -0800

    add inspect and start of throw functions

--- 2022/day11.py
+++ 2022/day11.py
@@ -5,37 +5,74 @@ from dataclasses import dataclass
 @dataclass()
 class Monkey:
     monkey_id: int
-    starting_items: list
+    items: list
     operation: str
     test_divisible_by: int
     if_true_throw_to: int
     if_false_throw_to: int
 
 
-def main() -> None:
-    input_data = get_data(day=11, year=2022).splitlines()
+def gen_monkey_list(input_data: str) -> list[Monkey]:
+    """
+    Generates a list of monkeys given the input data
+    :param input_data: a string with monkey information
+    :return: a list of monkey objects
+    """
     monkeys = []
-    my_data = ''.join(input_data)
-    my_data = my_data.split('Monkey')
-    for line in my_data:
-        line = line.strip()
-        if line:
-            monkey_id = int(line[0])
-            line = line.split('  ')
-            starting_items = line[1].split('Starting items: ')[1].split(', ')
-            operation = line[2].split('Operation: ')[1]
-            test = int(line[3].split('Test: ')[1][-1])
-            test_response = line[-3:]
-            if_true_throw_to = int(test_response[0][-1])
-            if_false_throw_to = int(test_response[2][-1])
-            monkeys.append(Monkey(monkey_id=monkey_id,
-                                  starting_items=starting_items,
-                                  operation=operation,
-                                  test_divisible_by=test,
-                                  if_true_throw_to=if_true_throw_to,
-                                  if_false_throw_to=if_false_throw_to))
+    my_data = input_data.split('Monkey ')
+    for monkey in my_data:
+        monkey = monkey.strip()
+        if not monkey:
+            continue
+        monkey_id = int(monkey[0])
+        monkey = monkey.splitlines()
+        items = monkey[1].split('  Starting items: ')[1].split(', ')
+        items = [eval(i) for i in items]
+        operation = monkey[2].split('  Operation: new = ')[1]
+        test = int(monkey[3].split('  Test: ')[1][-1])
+        test_response = monkey[-2:]
+        if_true_throw_to = int(test_response[0][-1])
+        if_false_throw_to = int(test_response[1][-1])
+        monkeys.append(Monkey(monkey_id=monkey_id,
+                              items=items,
+                              operation=operation,
+                              test_divisible_by=test,
+                              if_true_throw_to=if_true_throw_to,
+                              if_false_throw_to=if_false_throw_to))
+    return monkeys
+
+
+def inspect(monkey: Monkey) -> None:
+    """
+    Changes the monkey's items according to the monkey's operation
+    :param monkey: a monkey
+    :return: None
+    """
+    monkey.items = [eval(monkey.operation) for old in monkey.items]
+
+
+def throw(monkeys) -> None:
+    """
+    Tests divisibility and throws item to the appropriate monkey
+    :param monkeys: a lit of monkeys
+    :return: None
+    """
     for monkey in monkeys:
-        print(monkey)
+        for item in monkey.items:
+            if item % monkey.test_divisible_by == 0:
+                print(f'Throw to monkey {monkey.if_true_throw_to}')
+            else:
+                print(f'Throw to monkey {monkey.if_false_throw_to}')
+
+
+def main() -> None:
+    input_data = get_data(day=11, year=2022)
+    monkeys = gen_monkey_list(input_data)
+
+    for monkey in monkeys:
+        inspect(monkey)
+
+    throw(monkeys)
 
 
 if __name__ == '__main__':
