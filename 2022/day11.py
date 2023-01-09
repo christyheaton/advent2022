20980d3c0e56e4ae691411464aa874d61910f886
christyheaton <christyheaton@gmail.com>
2023-01-08 16:49:56 -0800

    Part 1 almost done

--- 2022/day11.py
+++ 2022/day11.py
@@ -10,6 +10,7 @@ class Monkey:
     test_divisible_by: int
     if_true_throw_to: int
     if_false_throw_to: int
+    monkey_business_factor: int = 0
 
 
 def gen_monkey_list(input_data: str) -> list[Monkey]:
@@ -29,7 +30,7 @@ def gen_monkey_list(input_data: str) -> list[Monkey]:
         items = monkey[1].split('  Starting items: ')[1].split(', ')
         items = [eval(i) for i in items]
         operation = monkey[2].split('  Operation: new = ')[1]
-        test = int(monkey[3].split('  Test: ')[1][-1])
+        test = int(monkey[3].split('  Test: ')[1].split()[-1])
         test_response = monkey[-2:]
         if_true_throw_to = int(test_response[0][-1])
         if_false_throw_to = int(test_response[1][-1])
@@ -48,31 +49,45 @@ def inspect(monkey: Monkey) -> None:
     :param monkey: a monkey
     :return: None
     """
-    monkey.items = [eval(monkey.operation) for old in monkey.items]
+    monkey.items = [int(eval(monkey.operation)/3) for old in monkey.items]
 
 
 def throw(monkeys) -> None:
     """
     Tests divisibility and throws item to the appropriate monkey
-    :param monkeys: a lit of monkeys
+    :param monkeys: a list of monkeys
     :return: None
     """
     for monkey in monkeys:
-        for item in monkey.items:
+        inspect(monkey)
+        print(f'Monkey {monkey.monkey_id} has {len(monkey.items)} items: {monkey.items}.')
+        while monkey.items:
+            monkey.monkey_business_factor +=1
+            item = monkey.items[0]
+            print(f'Current item is {item}')
             if item % monkey.test_divisible_by == 0:
-                print(f'Throw to monkey {monkey.if_true_throw_to}')
+                print(f'{item} is divisible by {monkey.test_divisible_by}')
+                print(f'Monkey {monkey.monkey_id} throws item {item} to monkey {monkey.if_true_throw_to}')
+                monkeys[monkey.if_true_throw_to].items.append(monkey.items.pop(0))
             else:
-                print(f'Throw to monkey {monkey.if_false_throw_to}')
+                print(f'{item} is not divisible by {monkey.test_divisible_by}')
+                print(f'Monkey {monkey.monkey_id} throws item {item} to monkey {monkey.if_false_throw_to}')
+                monkeys[monkey.if_false_throw_to].items.append(monkey.items.pop(0))
+            print(f'Monkey {monkey.monkey_id} now has {len(monkey.items)} items: {monkey.items}.')
 
 
 def main() -> None:
     input_data = get_data(day=11, year=2022)
+
+    with open('data/day11_test.txt') as f:
+        input_data = f.read()
     monkeys = gen_monkey_list(input_data)
 
-    for monkey in monkeys:
-        inspect(monkey)
+    for round in range(20):
+        print(f'Round {round}')
+        throw(monkeys)
 
-    throw(monkeys)
+    print(monkeys)
 
 
 if __name__ == '__main__':
