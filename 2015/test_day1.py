cabe8f6f037fd7e3c14a16549f944b90313b923b
christyheaton <christyheaton@gmail.com>
2023-01-15 07:01:06 -0800

    Add unit tests for 2015 day1

---
+++ 2015/test_day1.py
@@ -0,0 +1,23 @@
+import pytest
+from day1 import floor_count
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [('(())', 0),
+                          ('()()', 0),
+                          ('(((', 3),
+                          ('(()(()(', 3),
+                          ('))(((((', 3),
+                          ('())', -1),
+                          ('))(', -1),
+                          (')))', -3),
+                          (')())())', -3)])
+def test_floor_count(test_input, expected):
+    assert floor_count(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [(')', 1),
+                          ('()())', 5)])
+def test_floor_count_basement_index(test_input, expected):
+    assert floor_count(test_input, 2) == expected
