4272a1ec1faa0b109351e3176737ec6ed207f105
christyheaton <christyheaton@gmail.com>
2023-01-21 15:53:38 -0800

    unit tests for 2015 day2

---
+++ 2015/test_day2.py
@@ -0,0 +1,20 @@
+import pytest
+from day2 import total_wrapping_needed
+
+
+@pytest.fixture
+def test_input() -> list:
+    return ['2x3x4', '1x1x10']
+
+
+def test_part1(test_input: list) -> None:
+    assert total_wrapping_needed(test_input, 'paper') == 101
+
+
+def test_part2(test_input: list) -> None:
+    assert total_wrapping_needed(test_input, 'ribbon') == 48
+
+
+def test_val_error(test_input: list) -> None:
+    with pytest.raises(ValueError):
+        total_wrapping_needed(test_input, 'tape')
