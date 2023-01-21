cfc5e80fef4c50b9c7bb5b89ab772697c9f28375
christyheaton <christyheaton@gmail.com>
2023-01-21 05:47:56 -0800

    refactor into function

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,14 +1,16 @@
 from aocd import get_data
 
 
+def get_start_of_packet_marker(input_data: str, packet_length: int) -> int:
+    index = 0
+    while index < len(input_data):
+        sub_set = set(input_data[index:index + packet_length])
+        if len(sub_set) == packet_length:
+            return index + packet_length
+        index += 1
+
+
 if __name__ == '__main__':
     data = get_data(day=6, year=2022)
-    packet_length = 4  # use 4 for part 1, 14 for part 2
-    index = 0
-    while index < len(data):
-        sub_set = set(data[index:index + packet_length])
-        if len(sub_set) == packet_length:
-            print(f'First unique set of {packet_length} characters is {"".join(sub_set)}')
-            print(f'Found ending at index {index + packet_length}')
-            break
-        index += 1
+    print(f'Part 1: {get_start_of_packet_marker(data, 4)}')
+    print(f'Part 2: {get_start_of_packet_marker(data, 14)}')
