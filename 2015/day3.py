21f0c15be9229c1e07fe8c5aa7f26a2c14106a36
christyheaton <christyheaton@gmail.com>
2023-01-22 19:56:02 -0800

    part 2 solution

--- 2015/day3.py
+++ 2015/day3.py
@@ -1,28 +1,45 @@
 from aocd import get_data
 
 
-def houses_receiving_presents(input_data: str) -> int:
+def move(coordinates: tuple, direction: str) -> tuple:
+    x = coordinates[0]
+    y = coordinates[1]
+    match direction:
+        case '^':
+            y += 1
+        case 'v':
+            y -= 1
+        case '>':
+            x += 1
+        case '<':
+            x -= 1
+    return x, y
+
+
+def houses_receiving_presents(input_data: str) -> list:
     x = 0
     y = 0
     house_record = [(x, y)]
     for direction in input_data:
-        match direction:
-            case '^':
-                y += 1
-            case 'v':
-                y -= 1
-            case '>':
-                x += 1
-            case '<':
-                x -= 1
-        if (x, y) not in house_record:
-            house_record.append((x, y))
-    return len(house_record)
+        x, y = move((x, y), direction)
+        house_record.append((x, y))
+    return house_record
+
+
+def num_houses(visited_houses: list) -> int:
+    return len(list(set(visited_houses)))
 
 
 def main() -> None:
     data: str = get_data(day=3, year=2015)
-    print(houses_receiving_presents(data))
+    print(f'Part 1: {num_houses(houses_receiving_presents(data))}')
+
+    santa_instructions = data[::2]
+    robo_santa_instructions = data[1::2]
+    santa_houses = houses_receiving_presents(santa_instructions)
+    robo_santa_houses = houses_receiving_presents(robo_santa_instructions)
+
+    print(f'Part 2: {num_houses(santa_houses+robo_santa_houses)}')
 
 
 if __name__ == '__main__':
