93e590c38391c0d7ce4cb185fa5d71a7cc850698
christyheaton <christyheaton@gmail.com>
2023-01-22 19:56:40 -0800

    fixed unit tests for parts 1 and 2

--- 2015/test_day3.py
+++ 2015/test_day3.py
@@ -1,5 +1,5 @@
 import pytest
-from day3 import houses_receiving_presents
+from day3 import houses_receiving_presents, num_houses
 
 
 @pytest.mark.parametrize('test_input,expected',
@@ -7,12 +7,16 @@ from day3 import houses_receiving_presents
                           ('^>v<', 4),
                           ('^v^v^v^v^v', 2)])
 def test_part1(test_input, expected):
-    assert houses_receiving_presents(test_input) == expected
+    assert num_houses(houses_receiving_presents(test_input)) == expected
 
 
 @pytest.mark.parametrize('test_input,expected',
-                         [('>', 3),
+                         [('^>', 3),
                           ('^>v<', 3),
                           ('^v^v^v^v^v', 11)])
 def test_part2(test_input, expected):
-    assert houses_receiving_presents_robo(test_input) == expected
+    santa_instructions = test_input[::2]
+    robo_santa_instructions = test_input[1::2]
+    santa_houses = houses_receiving_presents(santa_instructions)
+    robo_santa_houses = houses_receiving_presents(robo_santa_instructions)
+    assert num_houses(santa_houses+robo_santa_houses) == expected
