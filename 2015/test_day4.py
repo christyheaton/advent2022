51b8c93bbd484008b35a79c13847ca5474908dcc
christyheaton <christyheaton@gmail.com>
2023-01-25 19:32:26 -0800

    unit tests for 2015 day4

---
+++ 2015/test_day4.py
@@ -0,0 +1,16 @@
+import pytest
+from day4 import check_md5_hash
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [('abcdef', 609043),
+                          ('pqrstuv', 1048970)])
+def test_part1(test_input, expected):
+    assert check_md5_hash(test_input, 5) == expected
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [('abcdef', 6742839),
+                          ('pqrstuv', 5714438)])
+def test_part2(test_input, expected):
+    assert check_md5_hash(test_input, 6) == expected
