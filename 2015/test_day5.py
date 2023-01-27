e50472104c39a3da2f8e11b8e5a76fde69ec97f2
christyheaton <christyheaton@gmail.com>
2023-01-27 05:12:35 -0800

    unit tests for part 2

--- 2015/test_day5.py
+++ 2015/test_day5.py
@@ -1,5 +1,5 @@
 import pytest
-from day5 import is_nice_part1
+from day5 import is_nice_part1, is_nice_part2, count_nice
 
 
 @pytest.mark.parametrize('test_input,expected',
@@ -10,3 +10,37 @@ from day5 import is_nice_part1
                           ('dvszwmarrgswjxmb', False)])
 def test_part1(test_input, expected):
     assert is_nice_part1(test_input) == expected
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [('qjhvhtzxzqqjkmpb', True),
+                          ('xxyxx', True),
+                          ('uurcxstgmygtbstg', False),
+                          ('ieodomkazucvgmuy', False)])
+def test_part2(test_input, expected):
+    assert is_nice_part2(test_input) == expected
+
+
+@pytest.fixture
+def test_input_pt1() -> list:
+    return ['ugknbfddgicrmopn',
+            'aaa',
+            'jchzalrnumimnmhp',
+            'haegwjzuvuyypxyu',
+            'dvszwmarrgswjxmb']
+
+
+def test_count_nice_pt1(test_input_pt1):
+    assert count_nice(test_input_pt1, 1) == 2
+
+
+@pytest.fixture
+def test_input_pt2() -> list:
+    return ['qjhvhtzxzqqjkmpb',
+            'xxyxx',
+            'uurcxstgmygtbstg',
+            'ieodomkazucvgmuy']
+
+
+def test_count_nice_pt2(test_input_pt2):
+    assert count_nice(test_input_pt2, 2) == 2
