4ed6f4b1ed5f30b2e4024a565cd0167d42cac812
christyheaton <christyheaton@gmail.com>
2023-01-28 12:08:57 -0800

    add np version

--- 2015/day6.py
+++ 2015/day6.py
@@ -1,4 +1,5 @@
 from aocd import get_data
+import numpy as np
 
 
 def create_grid(size: int) -> dict:
@@ -7,6 +8,10 @@ def create_grid(size: int) -> dict:
         for y in range(size):
             grid[(x, y)] = False
     return grid
+
+
+def create_npgrid(size):
+    return np.full((size, size), False)
 
 
 def parse_instructions(instruction: str) -> tuple:
@@ -33,11 +38,28 @@ def perform_instructions(instructions: list, grid: dict) -> dict:
     return grid
 
 
+def perform_inst_npgrid(instructions, npgrid):
+    for i in instructions:
+        inst, start_x, start_y, end_x, end_y = parse_instructions(i)
+        if inst == 'on':
+            npgrid[start_x:end_x + 1, start_y:end_y + 1] = True
+        elif inst == 'off':
+            npgrid[start_x:end_x + 1, start_y:end_y + 1] = False
+        else:
+            npgrid[start_x:end_x + 1,
+                   start_y:end_y + 1] = np.invert(npgrid[start_x:end_x + 1,
+                                                         start_y:end_y + 1])
+    return npgrid
+
+
 def main() -> None:
     input_data: list = get_data(day=6, year=2015).splitlines()
-    grid = create_grid(1_000)
-    perform_instructions(input_data, grid)
-    print(f'Part 1: {sum(grid.values())}')
+    # grid = create_grid(1_000)
+    # perform_instructions(input_data, grid)
+    # print(f'Part 1: {sum(grid.values())}')
+    grid = create_npgrid(1_000)
+    grid = perform_inst_npgrid(input_data, grid)
+    print(f'Part 1: {np.sum(grid)}')
 
 
 if __name__ == '__main__':
