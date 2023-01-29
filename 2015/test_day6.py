ad30cad9926371da21b8c99c0df885f6695939f9
christyheaton <christyheaton@gmail.com>
2023-01-28 16:15:15 -0800

    unit tests for 2015 day6

---
+++ 2015/test_day6.py
@@ -0,0 +1,41 @@
+import pytest
+import numpy as np
+from day6 import create_npgrid_pt1, create_npgrid_pt2, parse_instructions, \
+    perform_instructions_pt1, perform_instructions_pt2
+
+
+@pytest.mark.parametrize('test_input,expected',
+                         [('turn on 887,9 through 959,629', ('on', 887, 9, 959, 629)),
+                          ('turn on 454,398 through 844,448', ('on', 454, 398, 844, 448)),
+                          ('turn off 539,243 through 559,965', ('off', 539, 243, 559, 965)),
+                          ('toggle 720,196 through 897,994', ('toggle', 720, 196, 897, 994))])
+def test_parse_instructions(test_input, expected):
+    assert parse_instructions(test_input) == expected
+
+
+@pytest.fixture
+def test_instructions() -> list:
+    return ['turn on 1,2 through 3,4',
+            'turn on 0,0 through 1,3',
+            'toggle 4,2 through 5,3',
+            'turn on 2,1 through 4,3']
+
+
+def test_perform_instructions_pt1(test_instructions):
+    grid = create_npgrid_pt1(5)
+    grid = perform_instructions_pt1(test_instructions, grid)
+    assert np.array_equal(grid, np.array([[True, True, True, True, False],
+                                          [True, True, True, True, True],
+                                          [False, True, True, True, True],
+                                          [False, True, True, True, True],
+                                          [False, True, True, True, False]]))
+
+
+def test_perform_instructions_pt2(test_instructions):
+    grid = create_npgrid_pt2(5)
+    grid = perform_instructions_pt2(test_instructions, grid)
+    assert np.array_equal(grid, np.array([[1., 1., 1., 1., 0.],
+                                          [1., 1., 2., 2., 1.],
+                                          [0., 1., 2., 2., 1.],
+                                          [0., 1., 2., 2., 1.],
+                                          [0., 1., 3., 3., 0.]]))
