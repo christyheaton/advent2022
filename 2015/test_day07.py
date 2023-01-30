8d94506224b2a6f7807904b237860fa2eac6bab6
christyheaton <christyheaton@gmail.com>
2023-01-30 07:21:22 -0800

    unit tests for 2015 day7

---
+++ 2015/test_day07.py
@@ -0,0 +1,26 @@
+import pytest
+from day07 import circuit_switch
+
+
+@pytest.fixture
+def test_input() -> list:
+    return ['123 -> x',
+            '456 -> y',
+            'x AND y -> d',
+            'x OR y -> e',
+            'x LSHIFT 2 -> f',
+            'y RSHIFT 2 -> g',
+            'NOT x -> h',
+            'NOT y -> i']
+
+
+def test_switches(test_input) -> None:
+    res = circuit_switch(test_input)
+    assert res == {'x': 123,
+                   'y': 456,
+                   'd': 72,
+                   'e': 507,
+                   'f': 492,
+                   'g': 114,
+                   'h': 65412,
+                   'i': 65079}
