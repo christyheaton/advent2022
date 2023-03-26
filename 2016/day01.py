504aaf773f8ccdb63787916b18b7ee19011611a5
christyheaton <christyheaton@gmail.com>
2023-03-25 21:25:00 -0700

    write move method

--- 2016/day01.py
+++ 2016/day01.py
@@ -34,10 +34,18 @@ class Walker:
                 new_facing = DIRECTIONS[dir_index + 1]
         self.facing = new_facing
 
-    def move(self):
-        pass
+    def move(self, distance):
+        match self.facing:
+            case 'N':
+                self.position.y = self.position.y + distance
+            case 'E':
+                self.position.x = self.position.x + distance
+            case 'S':
+                self.position.y = self.position.y - distance
+            case 'W':
+                self.position.x = self.position.x - distance
 
-
+            
 def main() -> None:
     """start on part 1"""
     data = get_data(day=1, year=2016)
