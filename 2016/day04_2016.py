3a3e359c6997396f38c43fd4636a89c69562be96
christyheaton <christyheaton@gmail.com>
2023-10-02 16:56:35 -0700

    add part 2

--- 2016/day04_2016.py
+++ 2016/day04_2016.py
@@ -1,3 +1,4 @@
+from string import ascii_lowercase
 
 
 def parse(input: str) -> tuple:
@@ -29,9 +30,9 @@ def get_letter_counts(cypher_text: str) -> dict:
 
 def calc_checksum(letter_counts: dict) -> str:
     """
-
+    Calculates a checksum
     :param letter_counts:
-    :return:
+    :return: the checksum
     """
     ordered = sorted(letter_counts.items(), key=lambda x: x[1], reverse=True)
     checksum = ""
@@ -40,20 +41,36 @@ def calc_checksum(letter_counts: dict) -> str:
     return checksum
 
 
+def shift(message, offset):
+    """
+    A shift cypher algorithm
+    :param message: an encrypted room name
+    :param offset: the number of characters to shift
+    :return: a deciphered room name
+    """
+
+    trans = str.maketrans(ascii_lowercase,
+                          ascii_lowercase[offset:] + ascii_lowercase[:offset])
+    return message.lower().translate(trans)
+
 
 if __name__ == "__main__":
-    # data = ["aaaaa-bbb-z-y-x-123[abxyz]",
-    #         "a-b-c-d-e-f-g-h-987[abcde]",
-    #         "not-a-real-room-404[oarel]",
-    #         "totally-real-room-200[decoy]"]
     f = open("data/adventofcode.com_2016_day_4_input.txt", "r")
     data = f.read().splitlines()
 
+    # part 1
     sector_total = 0
+    real_rooms = []
     for d in data:
         name, sector_id, csum = parse(d)
         sorted_d = ''.join(sorted(name)).replace("-", "")
-        letter_counts = get_letter_counts(sorted_d)
-        if csum == calc_checksum(letter_counts):
+        my_letter_counts = get_letter_counts(sorted_d)
+        if csum == calc_checksum(my_letter_counts):
             sector_total += sector_id
+            real_rooms.append(f"{name}-{sector_id}")
     print(f"Part 1 solution: {sector_total}")
+
+    # part 2
+    decrypted_rooms = [shift(real_room, int(real_room[-3:])%26) for real_room in real_rooms]
+    north_pole_room = [room for room in decrypted_rooms if "northpole" in room][0]
+    print(f"Part 2 solution: {north_pole_room[-3:]}")
