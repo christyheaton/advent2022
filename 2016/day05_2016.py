d95461fa06249e19007d6fb8f2b5f094de5585d9
christyheaton <christyheaton@gmail.com>
2023-10-21 22:32:46 -0700

    refactor into class and add part 2

--- 2016/day05_2016.py
+++ 2016/day05_2016.py
@@ -3,28 +3,77 @@
 import hashlib
 
 
-def check_md5_hash(input_data, zeros):
-    """find md5 hash that starts with a configurable number of zeros"""
-    password = ''
-    count = 1
-    while True:
-        test = f'{input_data}{count}'
-        result = hashlib.md5(test.encode())
-        if result.hexdigest().startswith(zeros*'0'):
-            password = f'{password}{result.hexdigest()[5]}'
-            print(f'test: {test}')
-            print(f'result: {result.hexdigest()}')
-            print(f'password: {password}')
-        if len(password) == 8:
-            return password
-        count += 1
+class PasswordFinder:
+    def __init__(self, door_id: str):
+        """
+        Finds the password for a given door id.
+        :param door_id: The door id.
+        """
+        self.door_id = door_id
+
+    def get_md5_hash(self, count: int):
+        """
+        Get the md5 hash of the door id with count appended.
+        :param count: the increment to append to the door id.
+        :return: the md5 hash.
+        """
+        test = f'{self.door_id}{count}'
+        return hashlib.md5(test.encode())
+
+    @staticmethod
+    def check_interesting(hash_value) -> bool:
+        """
+        Check if the hash value is interesting, i.e. starts with 5 0s.
+        :param hash_value: The hash value
+        :return: True if interesting, otherwise False.
+        """
+        return hash_value.hexdigest().startswith(5 * '0')
+
+    def find_password_pt1(self) -> str:
+        """
+        Find password using Part 1 logic.
+        :return: The password.
+        """
+        password = ''
+        count = 0
+        while len(password) < 8:
+            hash_result = self.get_md5_hash(count)
+            if self.check_interesting(hash_result):
+                password += hash_result.hexdigest()[5]
+            count += 1
+        return password
+
+    def find_password_pt2(self) -> str:
+        """
+        Find password using Part 2 logic.
+        :return: The password.
+        """
+        password = ''
+        placements = {}
+        count = 0
+        while len(placements) < 8:
+            hash_result = self.get_md5_hash(count)
+            if self.check_interesting(hash_result):
+                hex_result = hash_result.hexdigest()
+                if hex_result[5].isnumeric():
+                    if int(hex_result[5]) < 8:
+                        if not placements.get(hex_result[5]):
+                            placements[hex_result[5]] = hex_result[6]
+            count += 1
+        for item in sorted(placements.items()):
+            password += str(item[1])
+        return password
 
 
 def main() -> None:
-    """solutions for parts 1 and 2"""
+    """
+    Print solutions for parts 1 and 2.
+    """
+    # input_data = 'abc' # test data
     input_data = 'cxdnnyjw'
-    res = check_md5_hash(input_data, 5)
-    print(res)
+    pw_finder = PasswordFinder(input_data)
+    print(f'Part 1: {pw_finder.find_password_pt1()}')
+    print(f'Part 2: {pw_finder.find_password_pt2()}')
 
 
 if __name__ == '__main__':
